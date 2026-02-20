72e9575a6b8383277f9fb569d9c1fef7413f123f
linachaika <aangelinaslonikhina@gmail.com>
2026-02-20 19:01:39 +0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+  // @TODO: Расчет выручки от операции
+  const { discount, sale_price: price, quantity } = purchase
+  const calculatedDiscount = 1 - discount / 100
+  const operationRevenue = price * quantity * calculatedDiscount
+  return operationRevenue
 }
 
 /**
@@ -15,8 +19,26 @@ function calculateSimpleRevenue(purchase, _product) {
  * @param seller карточка продавца
  * @returns {number}
  */
+
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+  let { profit } = seller
+  switch (index) {
+    case 0:
+      profit = 15
+      break
+    case 1:
+    case 2:
+      profit = 10
+      break
+    case total - 1:
+      profit = 0
+      break
+    default:
+      profit = 5
+      break
+  }
+  return profit
+  // @TODO: Расчет бонуса от позиции в рейтинге
 }
 
 /**
@@ -25,20 +47,86 @@ function calculateBonusByProfit(index, total, seller) {
  * @param options
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
+
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+  const { sellers, products, purchase_records: purchase } = data
+  const { calculateRevenue, calculateBonus } = options
+  const statsBySellerId = {}
 
-    // @TODO: Проверка наличия опций
+  const sellerIndex = sellers.reduce(
+    (result, seller) => ({
+      ...result,
+      [seller.id]: seller,
+    }),
+    {}
+  )
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+  const productIndex = products.reduce(
+    (result, product) => ({
+      ...result,
+      [product.sku]: product,
+    }),
+    {}
+  )
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+  purchase.forEach((record) => {
+    const sellerId = record.seller_id
+    const seller = sellerIndex[sellerId]
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+    if (!statsBySellerId[sellerId]) {
+      statsBySellerId[sellerId] = {
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {},
+      }
+    }
+    statsBySellerId[sellerId].sales_count += 1
 
-    // @TODO: Сортировка продавцов по прибыли
+    record.items.forEach((item) => {
+      const product = productIndex[item.sku]
+      const cost = product.purchase_price * item.quantity
+      const revenue = calculateSimpleRevenue(item, product)
+      const profitItem = revenue - cost
 
-    // @TODO: Назначение премий на основе ранжирования
+      statsBySellerId[sellerId].revenue += revenue
+      statsBySellerId[sellerId].profit += profitItem
 
-    // @TODO: Подготовка итоговой коллекции с нужными полями
+      if (!statsBySellerId[sellerId].products_sold[item.sku]) {
+        statsBySellerId[sellerId].products_sold[item.sku] = 0
+      }
+      statsBySellerId[sellerId].products_sold[item.sku] += item.quantity
+    })
+  })
+
+  const sellersStats = Object.values(statsBySellerId)
+
+  sellersStats.sort((a, b) => b.profit - a.profit)
+
+  sellersStats.forEach((seller, index) => {
+    seller.bonus = calculateBonusByProfit(index, sellersStats.length, seller)
+    const makeSellerTopProducts = (productsSold) => {
+      const productsObj = Object.entries(productsSold)
+      const productsArr = productsObj.map((item) => ({
+        sku: item[0],
+        quantity: item[1],
+      }))
+      productsArr.sort((a, b) => b.quantity - a.quantity)
+      return productsArr.slice(0, 10)
+    }
+    seller.top_products = makeSellerTopProducts(seller.products_sold)
+  })
+
+  const finalReportSellers = sellersStats.map((seller) => ({
+    seller_id: seller.id,
+    name: seller.name,
+    revenue: +seller.revenue.toFixed(2),
+    profit: +seller.profit.toFixed(2),
+    sales_count: seller.sales_count,
+    top_products: seller.top_products,
+    bonus: seller.bonus,
+  }))
+  return finalReportSellers
 }
